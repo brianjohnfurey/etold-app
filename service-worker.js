dfe870b47dd53b72f313848cc3c5d61b7b3cbd21
brianjohnfurey <brianjohnfurey@gmail.com>
2026-03-12 15:57:45 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='etold-v12';
+const CACHE='etold-v13';
 const CORE=['./','./index.html','./app-loader.js','./manifest.json','./offline.html','./code/loader-manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
