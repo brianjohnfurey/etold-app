7581cff4fd01099dd1d37065d6a6461165e29631
brianjohnfurey <brianjohnfurey@gmail.com>
2026-03-17 09:08:28 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,43 @@
-const CACHE='etold-v13';
-const CORE=['./','./index.html','./app-loader.js','./manifest.json','./offline.html','./code/loader-manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./offline.html'))));
+const CACHE_NAME = 'etold-v14-23';
+const PRECACHE = [
+  "./",
+  "index.html",
+  "app-loader.js",
+  "manifest.json",
+  "offline.html",
+  "assets/lion.png",
+  "icons/icon-180.png",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "code/loader-manifest.json",
+  "code/script0.part1.txt",
+  "code/script0.part2.txt",
+  "code/script0.part3.txt",
+  "code/script0.part4.txt",
+  "code/script0.part5.txt",
+  "code/script0.part6.txt",
+  "code/script1.part1.txt",
+  "code/script1.part2.txt",
+  "code/script2.part1.txt",
+  "code/script3.part1.txt",
+  "code/script4.part1.txt",
+  "code/script5.part1.txt",
+  "code/script6.part1.txt"
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : Promise.resolve()))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match('offline.html')))
+  );
 });
